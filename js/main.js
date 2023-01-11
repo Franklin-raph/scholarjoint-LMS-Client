6c0d4ae9edee5ac9760bc532c66c5ea0f7013a8f
Franklin-raph <igboekwulusifranklin@gmail.com>
2023-01-11 20:48:41 +0100

    added the courses section

--- js/main.js
+++ js/main.js
@@ -1,3 +1,52 @@
 function toggleNav(){
     document.querySelector('.hero ul').classList.toggle('showNav')
 }
+
+$(".courseSlider").slick({
+
+    // normal options...
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    dots: true,
+  
+    // the magic
+    responsive: [{
+        breakpoint: 1800,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true,
+        }
+  
+      },
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true,
+        }
+  
+      }, {
+  
+        breakpoint: 900,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          dots: true
+        }
+  
+      }, {
+  
+        breakpoint: 600,
+        settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            dots: true
+          }
+      }]
+  });
